df67e4c290e987c8953a9d021dcc66bda435e643
Cristian1823 <114130713+Cristian1823@users.noreply.github.com>
2023-03-27 12:15:41 -0500

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ window.addEventListener('load', () => {
 	// Agregar cada registro a la lista de registros
 	records.forEach(record => {
 		const li = document.createElement('li');
-		li.textContent = `${record.name} (${record.email}, ${record.phone})`;
+		li.textContent = `${record.name} (${record.country}, ${record.campaign}, ${record.venture}, ${record['coordinator-jr']}, ${record['report-hour']}, ${record.reporter})`;
 		recordsList.appendChild(li);
 	});
 });
@@ -21,30 +21,10 @@ form.addEventListener('submit', event => {
 
 	// Obtener los datos del formulario
 	const name = document.querySelector('#name').value;
-	const email = document.querySelector('#email').value;
-	const phone = document.querySelector('#phone').value;
-
-	// Crear un objeto de registro
-	const record = {
-		name: name,
-		email: email,
-		phone: phone
-	};
-
-	// Obtener los registros guardados del almacenamiento local
-	const records = JSON.parse(localStorage.getItem('records')) || [];
-
-	// Agregar el nuevo registro a la lista de registros
-	records.push(record);
-
-	// Guardar los registros en el almacenamiento local
-	localStorage.setItem('records', JSON.stringify(records));
-
-	// Agregar el nuevo registro a la lista en la página
-	const li = document.createElement('li');
-	li.textContent = `${record.name} (${record.email}, ${record.phone})`;
-	recordsList.appendChild(li);
-
-	// Limpiar el formulario
+	const country = document.querySelector('#country').value;
+	const campaign = document.querySelector('#campaign').value;
+	const venture = document.querySelector('#venture').value;
+	const coordinatorJr = document.querySelector('#coordinator-jr').value
+    	// Limpiar el formulario
 	form.reset();
 });
